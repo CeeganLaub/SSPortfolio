a29067008f726fbb7025079de075ffb31dda1569
Ceegan Laubscher <ceeganlaub@gmail.com>
2021-01-17 09:16:36 +0200

    Initial Commit

---
+++ db/migrate/20210114162436_create_transformations.rb
@@ -0,0 +1,10 @@
+class CreateTransformations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :transformations do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
